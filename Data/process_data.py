af88b8b73634556167fea484e2d051a41466beb3
Rona-star <rona.katuma@icloud.com>
2024-04-17 12:44:08 +0300

    Update process_data.py

--- Data/process_data.py
+++ Data/process_data.py
@@ -127,7 +127,7 @@ def main():
 
         print('Please provide the filepaths of the messages and categories datasets as the first and second argument respectively, as well as the filepath of the database to save the cleaned data to as the third argument.')
 
-        print('Example: python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db')
+        print('Example: python process_data.py messages.csv categories.csv DisasterResponse.db')
 
 if __name__ == '__main__':
 
